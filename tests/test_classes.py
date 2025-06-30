e1d0c5becceffa9e554af08c4da6e61adddd91fa
Andrew Serebrennikov <andrey_s_009941@mail.ru>
2025-07-01 04:24:47 +0800

    after black

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -6,6 +6,7 @@ from src.classes import Product, Category
 def product_value():
     return Product("Samsung S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
+
 def test_product(product_value):
     assert product_value.name == "Samsung S23 Ultra"
     assert product_value.description == "256GB, Серый цвет, 200MP камера"
@@ -15,13 +16,18 @@ def test_product(product_value):
 
 @pytest.fixture
 def category_value():
-    return Category("Смартфоны",
-                    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                    [])
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [],
+    )
 
 
 def test_category(category_value):
     assert category_value.name == "Смартфоны"
 
-    assert category_value.description == 'Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни'
+    assert (
+        category_value.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
     assert category_value.products == []
